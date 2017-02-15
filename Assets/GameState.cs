ec06a029a79cc9cca64ae891658505c4b7e9a7b0
William Davies <wd6g14@soton.ac.uk>
2017-02-15 19:27:36 +0000

    Initial work on grid system

---
+++ Assets/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class GameState : MonoBehaviour
+{
+    public Tile[][] GameGrid { get; }
+
+
+    public void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public enum MoveDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PlayerMoveInput()
+    {
+
+    }
+
+    /// <exception cref="ArgumentOutOfRangeException">Condition.</exception>
+    public Vector3 MoveTile(Transform player, MoveDirection moveDirection)
+    {
+        Vector3 currentPos = player.position;
+        Vector3 newPos = currentPos;
+
+        switch (moveDirection)
+        {
+            case MoveDirection.Up:
+                newPos.y = currentPos.y + 1;
+                break;
+            case MoveDirection.Down:
+                newPos.y = currentPos.y - 1;
+                break;
+            case MoveDirection.Left:
+                newPos.y = currentPos.x - 1;
+                break;
+            case MoveDirection.Right:
+                newPos.y = currentPos.x + 1;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("moveDirection", moveDirection, null);
+        }
+
+        return newPos;
+    }
+}
