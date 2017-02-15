ad842a2ff2160585d463c6b4c26354bc1f37fda9
William Davies <wd6g14@soton.ac.uk>
2017-02-15 20:08:29 +0000

    Commented out stuff that doesn't make sense just yet

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,57 +1,112 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
 {
-    public enum MoveDirection
+    private Vector2 pos;
+    private bool moving;
+
+//    #region Public Enums
+//
+//    public enum MoveDirection
+//    {
+//        Up,
+//        Down,
+//        Left,
+//        Right
+//    }
+
+//    #endregion Public Enums
+
+    #region Public Methods
+
+//    /// <exception cref="ArgumentOutOfRangeException">Condition.</exception>
+//    public Vector3 MoveTile(Transform player, MoveDirection moveDirection)
+//    {
+//        Vector3 currentPos = player.position;
+//
+//        Vector3 newPos = currentPos;
+//
+//        switch (moveDirection)
+//        {
+//            case MoveDirection.Up:
+//                newPos.y = currentPos.y + 1;
+//                break;
+//
+//            case MoveDirection.Down:
+//                newPos.y = currentPos.y - 1;
+//                break;
+//
+//            case MoveDirection.Left:
+//                newPos.y = currentPos.x - 1;
+//                break;
+//
+//            case MoveDirection.Right:
+//                newPos.y = currentPos.x + 1;
+//                break;
+//
+//            default:
+//                throw new ArgumentOutOfRangeException("moveDirection", moveDirection, null);
+//        }
+//
+//        return newPos;
+//    }
+
+    public void CheckInput()
     {
-        Up,
-        Down,
-        Left,
-        Right
+        // WASD control
+        // We add the direction to our position,
+        // this moves the character 1 unit (32 pixels)
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            pos += Vector2.right;
+            moving = true;
+        }
+
+        // For left, we have to subtract the direction
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            pos -= Vector2.right;
+            moving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pos += Vector2.up;
+            moving = true;
+        }
+
+        // Same as for the left, subtraction for down
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pos -= Vector2.up;
+            moving = true;
+        }
     }
 
+    #endregion Public Methods
+
+    #region Private Methods
+
     // Use this for initialization
-    void Start()
+    private void Start()
     {
+        // First store our current position when the
+        // script is initialized.
+        pos = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        CheckInput();
+
+        if (!moving) return;
+
+        // pos is changed when there's input from the player
+        transform.position = pos;
+        moving = false;
     }
 
-    public void PlayerMoveInput()
-    {
-
-    }
-
-    /// <exception cref="ArgumentOutOfRangeException">Condition.</exception>
-    public Vector3 MoveTile(Transform player, MoveDirection moveDirection)
-    {
-        Vector3 currentPos = player.position;
-        Vector3 newPos = currentPos;
-
-        switch (moveDirection)
-        {
-            case MoveDirection.Up:
-                newPos.y = currentPos.y + 1;
-                break;
-            case MoveDirection.Down:
-                newPos.y = currentPos.y - 1;
-                break;
-            case MoveDirection.Left:
-                newPos.y = currentPos.x - 1;
-                break;
-            case MoveDirection.Right:
-                newPos.y = currentPos.x + 1;
-                break;
-            default:
-                throw new ArgumentOutOfRangeException("moveDirection", moveDirection, null);
-        }
-
-        return newPos;
-    }
+    #endregion Private Methods
 }
