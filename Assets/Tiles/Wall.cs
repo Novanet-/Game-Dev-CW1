66ec9ef02ab37b3d17168e6a6b8464aac6437c02
Mark <m@aqu.me.uk>
2017-02-17 15:23:58 +0000

    Added Wall Tile

---
+++ Assets/Tiles/Wall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : Tile
+{
+
+    [SerializeField] private Sprite[] _sprites;
+
+    public new bool CanLandOn()
+    {
+        return false;
+    }
+}
