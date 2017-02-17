aeca246c1c36437f96ec92ca55256b9196ec66ed
Mark <m@aqu.me.uk>
2017-02-17 14:14:01 +0000

    Added Space to skip movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,7 @@ public class GameController : MonoBehaviour
 {
     #region Public Fields
 
-//    public Transform GameBoard;
+    //    public Transform GameBoard;
     public Tile[,] GameGrid;
     public GameObject TilePrefab, PlayerPrefab, CoinPrefab;
 
@@ -12,10 +12,14 @@ public class GameController : MonoBehaviour
 
     #region Private Fields
 
-    [SerializeField] private int _height;
-    [SerializeField] private int _width;
-    [SerializeField] private PlayerController[] _playerControllers;
-    [SerializeField] private int _activePlayer;
+    [SerializeField]
+    private int _height;
+    [SerializeField]
+    private int _width;
+    [SerializeField]
+    private PlayerController[] _playerControllers;
+    [SerializeField]
+    private int _activePlayer;
 
     #endregion Private Fields
 
@@ -51,18 +55,18 @@ public class GameController : MonoBehaviour
     {
         GameGrid = new Tile[Width, Height];
         for (var x = 0; x <= GameGrid.GetUpperBound(0); x++)
-        for (var y = 0; y <= GameGrid.GetUpperBound(1); y++)
-        {
-            GameObject tileInstance = Instantiate(TilePrefab, new Vector3(x, y, 0), Quaternion.identity);
-            GameGrid[x, y] = tileInstance.GetComponent<Tile>();
-            //TODO: Assign data to each tile when created, to have different tile types
-        }
+            for (var y = 0; y <= GameGrid.GetUpperBound(1); y++)
+            {
+                GameObject tileInstance = Instantiate(TilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                GameGrid[x, y] = tileInstance.GetComponent<Tile>();
+                //TODO: Assign data to each tile when created, to have different tile types
+            }
 
         _playerControllers = new PlayerController[4];
         _activePlayer = 0;
         for (var i = 0; i < _playerControllers.Length; i++)
         {
-             GameObject playerInstance = Instantiate(PlayerPrefab, new Vector3(i, i, 0), Quaternion.identity);
+            GameObject playerInstance = Instantiate(PlayerPrefab, new Vector3(i, i, 0), Quaternion.identity);
             _playerControllers[i] = playerInstance.GetComponent<PlayerController>();
         }
     }
@@ -80,28 +84,37 @@ public class GameController : MonoBehaviour
         // We add the direction to our position,
         // this moves the character 1 unit (32 pixels)
         Vector2 direction = Vector2.zero;
+        bool moved = false;
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction = Vector2.right;
+            moved = true;
         }
 
         // For left, we have to subtract the direction
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
+            moved = true;
         }
         else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction = Vector2.up;
+            moved = true;
         }
 
         // Same as for the left, subtraction for down
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction = Vector2.down;
+            moved = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moved = true;
         }
 
-        if (direction != Vector2.zero)
+        if (moved)
         {
             GetActivePlayer().Move(direction);
         }
@@ -127,11 +140,11 @@ public class GameController : MonoBehaviour
             do
             {
                 pos = new Vector3(Random.Range(0, this.Width), Random.Range(0, this.Height));
-                tile = GetGameTile((int) pos.x, (int) pos.y);
+                tile = GetGameTile((int)pos.x, (int)pos.y);
             } while (tile.Player != null && !tile.CanLandOn());
             Instantiate(CoinPrefab, pos, Quaternion.identity);
         }
-        
+
     }
 
     #endregion Private Methods
