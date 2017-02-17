70659a01c4d381507876df40e083f969eacf4a61
William Davies <wd6g14@soton.ac.uk>
2017-02-17 10:52:20 +0000

    Fixed player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,6 +38,7 @@ public class PlayerController : MonoBehaviour
     public void Move(Vector2 direction)
     {
         Debug.Log("" + _pos + " /" + direction + " /" + _gameController);
+        _pos = _pos + direction;
         _pos.x = Mathf.Clamp(_pos.x, 0, _gameController.Width - 1);
         _pos.y = Mathf.Clamp(_pos.y, 0, _gameController.Height - 1);
 
