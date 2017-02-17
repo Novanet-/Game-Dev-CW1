fcab0de36337074935e3df8bed7aed01f0283777
William Davies <wd6g14@soton.ac.uk>
2017-02-17 12:10:01 +0000

    Added tiled background

---
+++ Assets/Scripts/UnityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class UnityExtensions
+{
+    public static Vector3 ScreenToWorldLength(this Camera camera, Vector3 position)
+    {
+        return camera.ScreenToWorldPoint(position) - camera.ScreenToWorldPoint(Vector3.zero);
+    }
+}
