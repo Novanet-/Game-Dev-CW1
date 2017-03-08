773c0d1b50fdc605b7680992a1f843d929fe0706
Mark <m@aqu.me.uk>
2017-03-08 23:48:25 +0000

    added missed files

---
+++ Assets/Powerups/Powerup.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public abstract class Powerup : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start ()
+	{
+	    ToolTip = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public string ToolTip { get; private set; }
+
+    public abstract void Activate(GameController gameController, PlayerController currentPlayer);
+}
