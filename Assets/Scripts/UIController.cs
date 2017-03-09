1e87022bf5fc71ea5dd816f558f4dc1cf68e74fc
William Davies <wd6g14@soton.ac.uk>
2017-03-09 14:04:21 +0000

    Better palcement

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -178,8 +178,8 @@ namespace Assets.Scripts
             {
                 var x = Event.current.mousePosition.x;
                 var y = Event.current.mousePosition.y;
-                GUI.Label(new Rect(x - 160, y - 20, 300, 60), TooltipText, guiStyleBack);
-                GUI.Label(new Rect(x - 161, y - 20, 300, 60), TooltipText, guiStyleFore);
+                GUI.Label(new Rect(x - 120, y + 10, 300, 60), TooltipText, guiStyleBack);
+                GUI.Label(new Rect(x - 121, y + 10, 300, 60), TooltipText, guiStyleFore);
             }
         }
 
