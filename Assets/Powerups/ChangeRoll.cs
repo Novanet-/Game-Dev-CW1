06ae2a0d24ad8ba01fe7fba498669bd794396e96
Mark <m@aqu.me.uk>
2017-03-09 13:48:42 +0000

    Added Powerups

---
+++ Assets/Powerups/ChangeRoll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoll : Powerup{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
