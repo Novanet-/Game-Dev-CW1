d5152fa74ec7b5e3c880f141b0c319fb133cb3db
William Davies <wd6g14@soton.ac.uk>
2017-03-09 18:22:27 +0000

    Added random background music

---
+++ Assets/MusicController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _backgroundMusicArray;
+    // Use this for initialization
+    void Start()
+    {
+        int randomMusicIndex = Random.Range(0, _backgroundMusicArray.Length);
+        var audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = _backgroundMusicArray[randomMusicIndex];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
